c44e71ddbf35593c0925d3120034c3f6ef0364c3
Momna Batool <smomnabatool@gmail.com>
2026-03-12 22:19:24 +0000

    first commit

---
+++ cardly-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl text-white font-bold">
+        Cardly Setup Successful 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
